8001b3f9380b1bd6a0e552b16661646c77c2f46a
Tatsiana Dubinchyk <tatsiana_dubinchyk@epam.com>
2019-09-27 12:11:03 -0700

    2-add_search

--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -2,13 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class SingleBook extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         const books = this.props.books;
         const myBooks = books.map((item) => (
-            <div key={item.book_uri} item={item}>
+            <div key={item.book_uri}>
                 {item.title} by {item.author}
             </div>))
 
